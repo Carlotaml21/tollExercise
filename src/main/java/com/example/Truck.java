132e3d2c4aecb3ab561d1b9661526da6080d0ebc
Carlotaml21 <carlotaml21@gmail.com>
2025-03-28 11:36:09 +0100

    feat: add java class (TRUCK) @Override calculateToll (TDD:Green)

---
+++ src/main/java/com/example/Truck.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Truck extends Vehicle {
+    private int axles;
+
+    public Truck(String licensePlate, int axles) {
+        super(licensePlate);
+        this.axles = axles;
+    }
+    @Override
+    public int calculateToll(){
+        return axles * 50;
+    }
+}

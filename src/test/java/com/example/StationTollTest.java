51027a6bbc85d46b32f394dbcbe2091965321f78
Carlotaml21 <carlotaml21@gmail.com>
2025-03-28 10:28:00 +0100

    test: add test AddCarToTollStation() (TDD:RED)

---
+++ src/test/java/com/example/StationTollTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StationTollTest {
+    @Test
+    public void testAddCarToTollStation(){
+        StationToll station = new StationToll("peaje 1", "Asturias");
+        vehicle car = new Car ("1234");
+
+        station.processVehicle(car);
+
+        assertEquals(100,station.getTotalCollected());
+    }
+}
